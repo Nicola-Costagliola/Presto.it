5f91b7f048d23331ef24f51905b61d90fecbb09c
Nicola Costagliola <nicolacostagliola1996@gmail.com>
2024-03-05 19:02:53 +0100

    layout pronto da fare navbar e footer

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,3 +1,19 @@
-<div>
-    <!-- Simplicity is the consequence of refined emotions. - Jean D'Alembert -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ $title ?? config('app.name')}}</title>
+</head>
+<body>
+    <x-navbar />
+    
+    {{ $slot }}
+
+
+
+
+    <x-footer />
+</body>
+</html>
