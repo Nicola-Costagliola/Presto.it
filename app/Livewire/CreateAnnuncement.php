f4e2c183012947dedb06e0566886d15ac9b74a2d
Nicola Costagliola <nicolacostagliola1996@gmail.com>
2024-03-06 21:54:02 +0100

    card funzionanti

--- app/Livewire/CreateAnnuncement.php
+++ app/Livewire/CreateAnnuncement.php
@@ -15,12 +15,8 @@ class CreateAnnuncement extends Component
     public $body;
     #[Validate]
     public $price;
-<<<<<<< HEAD
-
-=======
 
     public $category= 1;
->>>>>>> 93844ba712a945518f0c97b94bcdce632393c937
 
 
     protected function rules() {
