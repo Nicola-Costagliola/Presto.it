fcad12fab74954146f01aa6ca496d132e899994b
DaniloBianchi <danilosmcr@gmail.com>
2024-03-08 01:28:02 +0100

    creata vista singolo articolo e sistemata un po l'estetica per la presentazione di domani(da rivedere insieme)

---
+++ resources/views/announcements/show.blade.php
@@ -0,0 +1,54 @@
+<x-layout>
+<div class="container text-center mt-3 p-2">
+    <div class="row">
+        <div class="col-12 shadow p-3">
+            <h1>Annuncio {{ $announcement->title }}</h1>
+        </div>
+    </div>
+</div>
+
+<div class="container text-center">
+    <div class="row">
+        <div class="col-12 shadow p-3">
+
+            <div id="carouselExampleFade" class="carousel slide carousel-fade">
+                <div class="carousel-inner">
+                    <div class="carousel-item active ">
+                        <img src="https://picsum.photos/1200/300" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://picsum.photos/1200/201" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://picsum.photos/1200/202" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://picsum.photos/1200/199" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+            {{-- fine carosello --}}
+            <div class="row">
+                <div class="col-12 mt-3">
+                    <h3>{{ $announcement->title }}</h3>
+                    <p class="card-text">Categoria: {{ $announcement->category->name }}</p>
+                    <p class="card-text">Descrizione: {{ $announcement->body }}</p>
+                    <p class="card-text"><strong>Prezzo: {{$announcement->price}} €</strong></p>
+                    <p><a href="{{ route('home') }}" class="btn shadow">&laquo; Indietro </a></p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+
+</x-layout>
