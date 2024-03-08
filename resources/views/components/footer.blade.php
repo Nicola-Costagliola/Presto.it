280c129baa3b36f89b8f8f6e41ea0bd7d2359ec9
Antonella Itollo <antoitollo@gmail.com>
2024-03-08 13:48:11 +0100

    modifiche footer front-end

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,23 +1,23 @@
-<footer class="bg-dark text-light text-center py-3 mt-5 container">
+<footer class="footer shadow text-light text-center py-3 mt-5 container">
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-6">
-                <h5>Contact Us</h5>
-                <p>Email: info@example.com</p>
-                <p>Phone: +123 456 789</p>
+                <h5 class="text-black">Contact Us</h5>
+                <p class="text-black">Email: info@example.com</p>
+                <p class="text-black">Phone: +123 456 789</p>
             </div>
             <div class="col-md-6">
-                <h5>Follow Us</h5>
+                <h5 class="text-black">Follow Us</h5>
                 <ul class="list-inline">
-                    <li class="list-inline-item"><a href="#">Facebook</a></li>
-                    <li class="list-inline-item"><a href="#">Twitter</a></li>
-                    <li class="list-inline-item"><a href="#">Instagram</a></li>
+                    <li class="list-inline-item link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover "><a href="#">Facebook</a></li>
+                    <li class=" list-inline-item link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"><a href="#">Twitter</a></li>
+                    <li class="list-inline-item link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"><a href="#">Instagram</a></li>
                 </ul>
             </div>
         </div>
     </div>
 
     <div class="text-center mt-3">
-        <p>Presto.it</p>
+        <p class="text-black">Presto.it</p>
     </div>
 </footer>
