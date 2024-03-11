a11eb677b37f44ba2e7fdf37d1882583646d1bac
Antonella Itollo <antoitollo@gmail.com>
2024-03-11 11:24:35 +0100

    modifiche card

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,5 +1,5 @@
-<!-- 
-    <div class="card shadow">
+
+    <!-- <div class="card shadow">
         <img src="https://picsum.photos/100/70" class="card-img-top p-3 rounded" alt="...">
         <div class="card-body">
             <h4 class="card-title text-center display-5 text-white">{{ $title }}</h4>
@@ -11,8 +11,7 @@
         </div>
     </div> -->
 
-    <div class="mb-3 ">
-    <div class="card shadow border border-primary" style="width: 18rem;">
+    <div class="card shadow border border-primary">
   <img src="https://picsum.photos/100/70" class="card-img-top rounded " alt="...">
   <div class="card-body">
   <h4 class="card-title text-center display-5 text_color_body montserrat">{{ $title }}</h4>
@@ -22,5 +21,4 @@
             <a href="{{ $route }}" class=" btn btn-sm btn-primary shadow text_color_body montserrat">Visualizza annuncio</a>
             <p class="card-footer mt-3 mb-0 text_color_body montserrat">Pubblicato il: {{ $created->format('d/m/Y') }}</p>
   </div>
-</div>
 </div>
