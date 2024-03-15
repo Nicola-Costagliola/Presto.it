d06a59a6ec9d8bfa682419f5f1b7ce73a90a8394
DaniloBianchi <danilosmcr@gmail.com>
2024-03-15 11:37:11 +0100

    sistemate rotte middleware web.php

--- routes/web.php
+++ routes/web.php
@@ -18,43 +18,34 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class,'home'])->name('home');
 Route::get('/categoria/{category}', [PageController::class, 'categoryShow'])->name('category.show');
-
 Route::get('/tutti/annunci', [AnnouncementController::class, 'showAll'])->name('announcements.showAll');
-
 Route::get('/dettaglio/annuncio/{announcement}', [AnnouncementController::class, 'showAnnouncement'])->name('announcements.show');
-
 Route::get('/category/dettaglio/annuncio/{announcement}', [PageController::class, 'showAnnouncement'])->name('category.single.announcement');
-
-// home revisore
-Route::get('revisor/home/{announcement}', [RevisorController::class, 'index'])->middleware('isRevisor')->name('revisor.index');
-
-Route::get('revisor/manage', [RevisorController::class, 'manage'])->middleware('isRevisor')->name('revisor.manage');
-
-// accetta annuncio
-Route::patch('/accetta/annuncio/{announcement}', [RevisorController::class, 'acceptAnnouncement'])->name('revisor.acceptAnnouncement');
-
-// rifiuta annuncio
-Route::patch('/rifiuta/annuncio/{announcement}', [RevisorController::class, 'rejectAnnouncement'])->name('revisor.rejectAnnouncement');
-
-Route::patch('/revisiona/annuncio/{announcement}', [RevisorController::class, 'reviewedAnnouncement'])->name('revisor.reviewedAnnouncement');
-
-// richiedi di diventare revisore
-Route::get('/richiesta/revisore', [RevisorController::class, 'becomeRevisor'])->middleware('auth')->name('become.revisor');
-Route::post('/richiesta/revisore/invia', [RevisorController::class, 'becomeRevisorSend'])->middleware('auth')->name('become.revisor.send');
-
-// rendi un utente revisore
-Route::get('/rendi/revisore/{user}', [RevisorController::class, 'makeRevisor'])->name('make.revisor');
-
 //ricerca annuncio
 Route::get('/ricerca/annuncio', [PageController::class, 'searchAnnouncements'])->name('announcements.search');
 
-
-
-
-
-
-
+//rotte auth
 Route::middleware('auth')->group( function () {
 
     Route::get('/nuovo/annuncio/', [AnnouncementController::class, 'createAnnouncement'])->name('announcements.create');
+    Route::get('/richiesta/revisore', [RevisorController::class, 'becomeRevisor'])->name('become.revisor');
+    // richiedi di diventare revisore
+    Route::post('/richiesta/revisore/invia', [RevisorController::class, 'becomeRevisorSend'])->name('become.revisor.send');
+    // rendi un utente revisore
+    Route::get('/rendi/revisore/{user}', [RevisorController::class, 'makeRevisor'])->name('make.revisor');
+});
+
+//rotte isRevisor
+Route::middleware('isRevisor')->group( function () {
+    // home revisore
+    Route::get('revisor/home/{announcement}', [RevisorController::class, 'index'])->name('revisor.index');
+    //gestione annunci
+    Route::get('revisor/manage', [RevisorController::class, 'manage'])->name('revisor.manage');
+    // accetta annuncio
+    Route::patch('/accetta/annuncio/{announcement}', [RevisorController::class, 'acceptAnnouncement'])->name('revisor.acceptAnnouncement');
+    // rifiuta annuncio
+    Route::patch('/rifiuta/annuncio/{announcement}', [RevisorController::class, 'rejectAnnouncement'])->name('revisor.rejectAnnouncement');
+    //rivedi annuncio
+    Route::patch('/revisiona/annuncio/{announcement}', [RevisorController::class, 'reviewedAnnouncement'])->name('revisor.reviewedAnnouncement');
+
 });
