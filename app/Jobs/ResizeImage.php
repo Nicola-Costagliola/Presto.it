04f7589d1247f548e455ad98942b5a24c62aada3
DaniloBianchi <danilosmcr@gmail.com>
2024-03-20 13:10:52 +0100

    Story 6 completa e funzionante

--- app/Jobs/ResizeImage.php
+++ app/Jobs/ResizeImage.php
@@ -39,8 +39,8 @@ class ResizeImage implements ShouldQueue
     {
         $w = $this->w;
         $h = $this->h;
-        $srcPath= storage_path() . '/app/puplic/' . $this->path . '/' . $this->fileName;
-        $destPath= storage_path() . '/app/puplic/' . $this->path . "/crop_{$w}x{$h}_" . $this->fileName;
+        $srcPath= storage_path() . '/app/public/' . $this->path . '/' . $this->fileName;
+        $destPath= storage_path() . '/app/public/' . $this->path . "/crop_{$w}x{$h}_" . $this->fileName;
 
         $croppedImage = Image::load($srcPath)
                         ->crop(Manipulations::CROP_CENTER, $w, $h)
