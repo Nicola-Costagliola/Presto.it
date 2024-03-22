015f1820130c97ad41e54a87a1f84d185fb12a29
Antonella Itollo <antoitollo@gmail.com>
2024-03-22 15:12:30 +0100

    front-end home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 <x-layout>
-  <div class="container-fluid py-5 mb-5">
+
+  <div class="container-fluid py-5 mb-5 hero-header background">
     <div class="container py-5">
       <div class="row g-5 align-items-center ">
         <div class="col-12 col-lg-7">
@@ -8,9 +9,9 @@
 
           <div class="position-relative mx-auto">
             <form class="d-flex " role="search" action="{{ route('announcements.search')}}" method="GET">
-              <input class="form-control" name="searched" type="search" placeholder="Cerca"
+              <input class="form-control border-2 border-secondary w-75 py-3 px-4 rounded-pill" name="searched" type="search" placeholder="Cerca"
               aria-label="Search">
-              <button class="btn btn-outline text-white montserrat" type="submit">Cerca</button>
+              <button class="btn btn-primary border-2 border-secondary py-3 px-4 position-absolute rounded-pill text-black h-100" style="top: 0; right: 25%;" type="submit">Cerca</button>
             </form>
           </div>
         </div>
@@ -25,7 +26,7 @@
               <div class="carousel-inner" role="listbox">
                 <div class="carousel-item @if($loop->first)active @endif rounded">
                   <img src="https://picsum.photos/400/300" alt="" class="img-fluid w-100 h-100 bg-secondary rounded">
-                  <a href="{{ route('category.show', $category ) }}" class="btn px-4 py-2 text-white rounded ">{{$category->name}}</a>
+                  <a href="{{ route('category.show', $category ) }}" class="btn px-4 py-2 text-white rounded">{{$category->name}}</a>
                 </div>
               </div>
               <button class="carousel-control-prev" type="button" data-bs-target="#carouselId" data-bs-slide="prev">
