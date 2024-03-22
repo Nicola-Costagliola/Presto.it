ea594565431fa3602b6f1ff251c9937a55d1e48d
DaniloBianchi <danilosmcr@gmail.com>
2024-03-22 12:35:11 +0100

    inserito ricerca e carosello categorie home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,51 @@
 <x-layout>
+  <div class="container-fluid py-5 mb-5">
+    <div class="container py-5">
+      <div class="row g-5 align-items-center ">
+        <div class="col-12 col-lg-7">
+          <h1 class="mb-5 display-3 text-primary">Pesto.it</h1>
+          <h4 class="mb-3 text-secondary">Con Presto.it trovi subito quello che cerchi</h4>
+
+          <div class="position-relative mx-auto">
+            <form class="d-flex " role="search" action="{{ route('announcements.search')}}" method="GET">
+              <input class="form-control" name="searched" type="search" placeholder="Cerca"
+              aria-label="Search">
+              <button class="btn btn-outline text-white montserrat" type="submit">Cerca</button>
+            </form>
+          </div>
+        </div>
+
+
+          <div class="col-12 col-lg-5">
+            <div id="carouselId" class="carousel slide position-relative" data-bs-ride="carousel">
+
+              @foreach ($categories as $category)
+
+
+              <div class="carousel-inner" role="listbox">
+                <div class="carousel-item @if($loop->first)active @endif rounded">
+                  <img src="https://picsum.photos/400/300" alt="" class="img-fluid w-100 h-100 bg-secondary rounded">
+                  <a href="{{ route('category.show', $category ) }}" class="btn px-4 py-2 text-white rounded ">{{$category->name}}</a>
+                </div>
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselId" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon bg-black " aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselId" data-bs-slide="next">
+                <span class="carousel-control-next-icon bg-black " aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+              @endforeach
+
+            </div>
+          </div>
+
+      </div>
+    </div>
+  </div>
+
+
   <div class="container mb-5 text-center mt-3 p-5">
 
 
@@ -6,29 +53,22 @@
 
       <div class="col-12 mb-5 p-2 ">
 
-          @if(session()->has('access.denied'))
-          <div class="alert alert-danger montserrat">
-            {{ session('access.denied') }}
-          </div>
-          @endif
-
-          @if(session()->has('message'))
-          <div class="alert alert-success montserrat">
-            {{ session('message') }}
-          </div>
-          @endif
-
-        <div class="z-4 position-absolute p-5 m-5 ">
-          <h1 class="tracking-in-expand display-1  ">Presto.it</h1>
+        @if(session()->has('access.denied'))
+        <div class="alert alert-danger montserrat">
+          {{ session('access.denied') }}
         </div>
-        <img  class="img-fluid opacity-50 card-img " src="https://picsum.photos/2000/1000" alt="" >
+        @endif
+
+        @if(session()->has('message'))
+        <div class="alert alert-success montserrat">
+          {{ session('message') }}
+        </div>
+        @endif
+
+
 
       </div>
       <div class="col-12 shadow p-5 reveal reveal.active">
-
-
-
-
 
         <h1 class="display-3 text_color montserrat">{{__('messages.welcome')}}</h1>
 
@@ -41,19 +81,19 @@
     <div class="row mt-5 shadow p-5 g-3">
       @foreach($announcements as $announcement)
       <div class="col-12 col-xl-4 col-lg-4 col-md-6 align-content-center ">
-          <x-card 
-          :title="$announcement->title"
-          :img="!$announcement->images()->get()->isEmpty() 
-          ? $announcement->images()->first()->getUrl(400,300)
-          : 'https://picsum.photos/200'"
-          :category="$announcement->category->name"
-          :body="$announcement->body"
-          :price="$announcement->price"
-          :created="$announcement->created_at"
-          :route="route('announcements.show', $announcement)"
-          />
-        </div>
-          @endforeach
+        <x-card
+        :title="$announcement->title"
+        :img="!$announcement->images()->get()->isEmpty()
+        ? $announcement->images()->first()->getUrl(400,300)
+        : 'https://picsum.photos/200'"
+        :category="$announcement->category->name"
+        :body="$announcement->body"
+        :price="$announcement->price"
+        :created="$announcement->created_at"
+        :route="route('announcements.show', $announcement)"
+        />
+      </div>
+      @endforeach
     </div>
   </div>
 
