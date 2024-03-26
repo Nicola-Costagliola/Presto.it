62b5dc7afec9daead2b613fc12687625a8b2f369
DaniloBianchi <danilosmcr@gmail.com>
2024-03-26 20:56:42 +0100

    sistemato carosello revisore

--- resources/views/livewire/revisor-list.blade.php
+++ resources/views/livewire/revisor-list.blade.php
@@ -22,16 +22,16 @@
                             <h2 class="accordion-header ">
                                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                                 data-bs-target="#flush-collapse{{$announcement->id}}" aria-expanded="false" aria-controls="flush-collapse{{$announcement->id}}">
-                                    <div class="col-6">
-                                        {{ $announcement->title }}
-                                    </div>
-                                    <div class="col-6">
-                                        @if($announcement->is_accepted === null)
-                                        <span class="text-warning">Da revisonare</span>
-                                        @elseif ($announcement->is_accepted === 0)
-                                        <span class="text-danger">Scartato</span>
-                                        @endif
-                                    </div>
+                                <div class="col-6">
+                                    {{ $announcement->title }}
+                                </div>
+                                <div class="col-6">
+                                    @if($announcement->is_accepted === null)
+                                    <span class="text-warning">Da revisonare</span>
+                                    @elseif ($announcement->is_accepted === 0)
+                                    <span class="text-danger">Scartato</span>
+                                    @endif
+                                </div>
                                 </button>
                             </h2>
 
@@ -44,62 +44,62 @@
                                         {{-- Carosello --}}
                                         <div class="col-12 col-md-9 my-auto p-1">
 
-                                            <div id="showCarousel" class="carousel slide" data-bs-ride="carousel">
+                                            <div id="showCarousel" class="carousel slide shadow" data-bs-ride="carousel">
 
                                                 @if($announcement->images->isNotEmpty())
-                                                @foreach($announcement->images as $image)
-                                                <div class="carousel-inner">
-                                                    <div class="row">
-                                                        <div class="carousel-item d-md-flex   @if($loop->first)active @endif">
-                                                            <div class="col-12 col-md-6">
-                                                                <img src="{{ $image->getUrl(400,300) }}" class=" d-block w-100 p-1 rounded" alt="...">
-                                                            </div>
-                                                            <div class="col-12 col-md-4 ps-2 my-auto text-center ">
-                                                                <h5>Revisione immagini:</h5>
-                                                                <p>Adulti: <span class="{{ $image->adult }}"></span></p>
-                                                                <p>Satira: <span class="{{ $image->spoof }}"></span></p>
-                                                                <p>Medicina: <span class="{{ $image->medical }}"></span></p>
-                                                                <p>Violenza: <span class="{{ $image->violence }}"></span></p>
-                                                                <p>Contenuto sessuale: <span class="{{ $image->racy }}"></span></p>
-                                                            </div>
-                                                            <div class="col-12 col-md-2 my-auto mx-auto  ">
-                                                                <h5 class="">Tags</h5>
-                                                                <div class=" p-2 ">
-                                                                         @if($image->labels)
+                                                    <div class="carousel-inner ">
+                                                        @foreach($announcement->images as $image)
+                                                            <div class="carousel-item  @if($loop->first)active   @endif">
+                                                                <div class="row">
+
+                                                                    <div class="col-12 col-md-6">
+                                                                        <img src="{{ $image->getUrl(400,300) }}" class="w-100 rounded" alt="...">
+                                                                    </div>
+                                                                    <div class="col-12 col-md-4  text-center text-md-start ">
+                                                                        <h5>Revisione immagini:</h5>
+                                                                        <p>Adulti: <span class="{{ $image->adult }}"></span></p>
+                                                                        <p>Satira: <span class="{{ $image->spoof }}"></span></p>
+                                                                        <p>Medicina: <span class="{{ $image->medical }}"></span></p>
+                                                                        <p>Violenza: <span class="{{ $image->violence }}"></span></p>
+                                                                        <p>Contenuto sessuale: <span class="{{ $image->racy }}"></span></p>
+                                                                    </div>
+                                                                    <div class="col-12 col-md-2  text-center  text-md-start">
+                                                                        <h5 class="">Tags</h5>
+                                                                        <div class="">
+                                                                            @if($image->labels)
                                                                             @foreach($image->labels as $label)
-                                                                                <p class=" d-inline ">{{ $label}} </p>
+                                                                            <p class=" d-inline ">{{ $label}} </p>
                                                                             @endforeach
-                                                                        @endif
+                                                                            @endif
+                                                                        </div>
+                                                                    </div>
+
                                                                 </div>
                                                             </div>
+                                                        @endforeach
+                                                    </div>
+                                                @else
+                                                    <div class="carousel-inner">
+                                                        <div class="carousel-item active">
+                                                            <img src="https://picsum.photos/400/300" class="d-block w-100  p-1 rounded " alt="...">
+                                                        </div>
+                                                        <div class="carousel-item">
+                                                            <img src="https://picsum.photos/401/300" class=" d-block w-100 p-1 rounded " alt="...">
                                                         </div>
                                                     </div>
-                                                    @endforeach
-                                                </div>
-                                                @else
-                                                <div class="carousel-inner">
-                                                    <div class="carousel-item active">
-                                                        <img src="https://picsum.photos/400/300" class="d-block w-100  p-1 rounded " alt="...">
-                                                    </div>
-                                                    <div class="carousel-item">
-                                                        <img src="https://picsum.photos/401/300" class=" d-block w-100 p-1 rounded " alt="...">
-                                                    </div>
-                                                </div>
                                                 @endif
 
-                                                    <button class="carousel-control-prev" type="button" data-bs-target="#showCarousel"
-                                                    data-bs-slide="prev">
-                                                    <span class="bi bi-caret-left-fill text-black fs-1 " aria-hidden="true"></span>
-                                                    <span class="visually-hidden">Previous</span>
-                                                    </button>
-                                                    <button class="carousel-control-next" type="button" data-bs-target="#showCarousel"
-                                                    data-bs-slide="next">
-                                                    <span class="bi bi-caret-right-fill text-black fs-1 " aria-hidden="true"></span>
-                                                    <span class="visually-hidden">Next</span>
-                                                    </button>
-
+                                                <button class="carousel-control-prev" type="button" data-bs-target="#showCarousel"
+                                                data-bs-slide="prev">
+                                                <span class="bi bi-caret-left-fill text-black fs-1 " aria-hidden="true"></span>
+                                                <span class="visually-hidden">Previous</span>
+                                                </button>
+                                                <button class="carousel-control-next" type="button" data-bs-target="#showCarousel"
+                                                data-bs-slide="next">
+                                                <span class="bi bi-caret-right-fill text-black fs-1 " aria-hidden="true"></span>
+                                                <span class="visually-hidden">Next</span>
+                                                </button>
                                             </div>
-
 
                                         </div>
 
@@ -112,30 +112,29 @@
                                             <p>Autore: <span class="fs-5">{{ $announcement->user->name }}</span></p>
                                             <p>Data creazione:: <span class="fs-5">{{ $announcement->created_at->format('d/m/Y') }}</span>
                                             </p>
-
                                         </div>
 
                                         {{-- Bottoni --}}
-                                        <div class="col-12 col-md-1 mt-md-auto ms-md-auto me-4 d-flex justify-content-around  d-md-block ">
+                                        <div class="col-12 d-flex justify-content-center mt-2  ">
 
                                             <form action="{{ route('revisor.acceptAnnouncement', $announcement) }}" method="POST">
                                                 @csrf
                                                 @method('PATCH')
-                                                <button type="submit" class="btn btn-success shadow mb-4">Accetta</button>
+                                                <button type="submit" class="btn btn-success shadow">Accetta</button>
                                             </form>
 
                                             <form action="{{ route('revisor.rejectAnnouncement',$announcement) }}" method="POST">
                                                 @csrf
                                                 @method('PATCH')
-                                                <button type="submit" class="btn btn-success shadow mb-2">Rifiuta</button>
+                                                <button type="submit" class="btn btn-success shadow ms-2">Rifiuta</button>
                                             </form>
+                                        </div>
 
                                     </div>
 
                                 </div>
 
                             </div>
-
                         </div>
                     </div>
 
