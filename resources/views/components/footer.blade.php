fa95ca0d588eaf4605a1abfe5604935a64f361c0
DaniloBianchi <danilosmcr@gmail.com>
2024-03-28 16:51:57 +0100

    cambiato colore testo del footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -9,17 +9,17 @@
         <div class="row">
 
              <div class="col-6">
-                <h5 class="text-white montserrat">{{ __('messages.contattaci') }}</h5>
-                <p class="text-white bi bi-envelope-fill montserrat"> Email: info@example.com</p>
-                <p class="text-white bi bi-telephone-fill montserrat">{{ __('messages.telefono') }}: +123 456 789</p>
+                <h5 class="text_color_body montserrat">{{ __('messages.contattaci') }}</h5>
+                <p class="text_color_body bi bi-envelope-fill montserrat"> Email: info@example.com</p>
+                <p class="text_color_body bi bi-telephone-fill montserrat">{{ __('messages.telefono') }}: +123 456 789</p>
              </div>
 
             @auth
                 @if(!Auth::user()->is_revisor)
 
                     <div class="col-md-6">
-                        <h5 class="montserrat text-white">{{ __('messages.lavoraConNoi') }}</h5>
-                        <p class="montserrat text-white">{{ __('messages.registratiEClick') }}</p>
+                        <h5 class="montserrat text_color_body">{{ __('messages.lavoraConNoi') }}</h5>
+                        <p class="montserrat text_color_body">{{ __('messages.registratiEClick') }}</p>
                         <a href="{{ route('become.revisor') }}" class="btn btn-primary text-light shadow my-3 montserrat">{{ __('messages.diventaRevisore') }}</a>
                     </div>
 
@@ -27,11 +27,11 @@
             @endauth
 
 
-            <div class="col-md-6">
-                <h5 class="text-white montserrat">{{ __('messages.seguici') }}</h5>
-                    <li class="list-inline-item link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover bi bi-facebook text-white montserrat"><a href="#"> Facebook</a></li>
-                    <li class=" list-inline-item link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover bi bi-telegram text-white montserrat"><a href="#"> Twitter</a></li>
-                    <li class="list-inline-item link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover bi bi-instagram text-white montserrat"><a href="#"> Instagram</a></li>
+            <div class="col-md-6 ">
+                <h5 class="text_color_body montserrat">{{ __('messages.seguici') }}</h5>
+                    <li class="list-inline-item link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover bi bi-facebook text_color_body montserrat"><a href="#" class="text_color_body"> Facebook</a></li>
+                    <li class=" list-inline-item link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover bi bi-telegram text_color_body montserrat"><a href="#" class="text_color_body"> Twitter</a></li>
+                    <li class="list-inline-item link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover bi bi-instagram text_color_body montserrat"><a href="#" class="text_color_body"> Instagram</a></li>
                 </ul>
             </div>
         </div>
