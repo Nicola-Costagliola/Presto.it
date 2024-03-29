5d93a99346b21d5d6de66e4d6ee215c31d733668
DaniloBianchi <danilosmcr@gmail.com>
2024-03-29 11:59:07 +0100

    cambiato colore placeholder form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,55 +1,4 @@
 <x-layout>
-    <!-- <div class="container ">
-
-        <div class="row mt-5  ">
-
-            <h1 class=" mt-3  text-center display-5 text_color mt-5">{{ __('messages.accedi') }}</h1>
-
-            <div class=" col-12 col-lg-6 mx-auto ">
-                <div class=" form card mt-3  shadow">
-
-                    <div class=" card-header text_color_body bi bi-person-circle ">{{ __('messages.accedi') }}
-                    </div>
-                    <div class=" card-body ">
-                        <form action="/login" method="POST">
-                            @csrf
-                            <div class="row g-3 ">
-                                <div class="col-12">
-                                    <label class="text_color_body bi bi-envelope" for="email"> Email</label>
-                                    <p class="fw-lighter m-0 p-0 ">{{ __('messages.emailObb') }}</p>
-                                    <input type="email" id="email" name="email" class="form-control @error('email') is-invalid @enderror" value=" {{ old('email') }} ">
-                                    @error('email') <span class=" text-danger small "> {{ $message }} </span>  @enderror
-                                </div>
-                                <div class="col-12 ">
-                                    <label for="password" class="text_color_body bi bi-lock-fill"> Password</label>
-                                    <p class="fw-lighter m-0 p-0 ">{{ __('messages.passwordObb') }}</p>
-                                    <input type="password" id="password" name="password" class="form-control @error('password') is-invalid @enderror">
-                                    
-                                    @error('password') <span class=" text-danger small "> {{ $message }} </span>  @enderror
-                                </div>
-                                <div class="col-12">
-                                    <label for="remember" class="text_color_body bi bi-lock-fill">{{ __('messages.remember') }}</label>
-                                    <input type="checkbox" id="remember" name="remember" class="" value="true">
-                                </div>
-                                <div class="col-12">
-                                    <button type="submit" class=" btn btn-primary text_color_body ">{{ __('messages.entra') }}
-                                    </button>
-
-                                </div>
-
-                            </div>
-                        </form>
-
-                    </div>
-
-                </div>
-
-            </div>
-
-            <x-back />
-
-        </div>
-    </div> -->
 
     <x-back />
 
@@ -68,7 +17,7 @@
 
             <div class="col-6 mt-4">
             <span class="mt-5 ">Non sei ancora registrato?</span>
-                <a href="/register" class=" btn shadow mx-0 ">Registrati</a>
+                <a href="/register" class=" btn shadow mx-0 text-white">Registrati</a>
             </div>
         </div>
 
@@ -80,7 +29,7 @@
                 </span>
 
                 <div class="wrap-input100 validate-input">
-                    
+
                     <label class="" for="email"> Email
                         <i class="bi bi-envelope"></i>
                     </label>
@@ -107,7 +56,7 @@
 
                 <button type="submit" class=" contact100-form-btn ">{{ __('messages.entra') }}
                 </button>
-                
+
             </form>
 
         </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,64 +1,13 @@
 <x-layout>
-    <!-- <div class="container">
-        <div class="row mt-5 ">
-            <h1 class=" mt-3 text-center display-5 text_color ">{{ __('messages.registrati') }}</h1>
-            <div class="col-12 col-lg-6  mx-auto ">
-                <div class=" form card mt-3  shadow">
-                    <div class=" card-header text_color_body bi bi-person-circle">
-                        {{ __('messages.registrati') }}
-                    </div>
-                    <div class=" card-body ">
-                        <form action="/register" method="POST">
-                            @csrf
-                            <div class="row g-3 ">
-                                <div class="col-12">
-                                    <label for="name" class="text_color_body bi bi-person">{{ __('messages.inserisciNome') }}</label>
-                                    <p class="fw-lighter m-0 p-0 ">{{ __('messages.nomeObb') }}</p>
-                                    <input type="text" id="name" name="name" class="form-control @error('name') is-invalid @enderror" value=" {{ old('name') }} ">
-                                    @error('name') <span class=" text-danger small "> {{ __('messages.name') }} </span>  @enderror
-                                </div>
-                                <div class="col-12">
-                                    <label for="email" class="text_color_body bi bi-envelope">{{ __('messages.inserisciEmail') }}</label>
-                                    <p class="fw-lighter m-0 p-0 ">{{ __('messages.emailObb') }}</p>
-                                    <input type="email" id="email" name="email" class="form-control @error('email') is-invalid @enderror" value=" {{ old('email') }} ">
-                                    @error('email') <span class=" text-danger small "> {{ $message }} </span>  @enderror
-                                </div>
-                                <div class="col-12">
-                                    <label for="password" class="text_color_body bi bi-lock-fill">{{ __('messages.inserisciPassword') }}</label>
-                                    <p class="fw-lighter m-0 p-0 ">{{ __('messages.passwordObb') }}</p>
-                                    <input type="password" id="password" name="password" class="form-control @error('password') is-invalid @enderror">
-                                    @error('password') <span class=" text-danger small "> {{ $message }} </span>  @enderror
-                                </div>
-                                <div class="col-12">
-                                    <label for="password_confirmation" class="text_color_body bi bi-lock-fill">{{ __('messages.confermaPassword') }}</label>
-                                    <p class="fw-lighter m-0 p-0 ">{{ __('messages.confPasswordObb') }}</p>
-                                    <input type="password" id="password_confirmation" name="password_confirmation" class="form-control @error('password') is-invalid @enderror">
-                                    @error('password') <span class=" text-danger small "> {{ $message }} </span>  @enderror
-                                </div>
-                                <div class="col-12">
-                                    <button type="submit" class=" btn btn-primary text_color_body ">{{ __('messages.registrati') }}
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-
-        <x-back />
-
-    </div> -->
 
     <x-back />
 
-    <div class="container-contact100">
+    <div class="container-contact100 ">
 
 
-        <div class="wrap-contact100">
+        <div class="wrap-contact100 shadow">
 
-       
+
         <div class="row">
             <div class="col-6">
                 <a class="navbar-brand d-flex align-items-center " href="{{ route('home') }}">
@@ -70,7 +19,7 @@
 
             <div class="col-6 mt-4 ">
                 <span class="mx-auto">Hai già un account?</span>
-                <a href="/login" class=" btn shadow mx-5  ">Accedi</a>
+                <a href="/login" class=" btn shadow mx-5 text-white ">Accedi</a>
             </div>
         </div>
 
@@ -93,7 +42,7 @@
                 </div>
 
                 <div class="wrap-input100 validate-input">
-                    
+
                     <label class="" for="email"> Email
                         <i class="bi bi-envelope"></i>
                     </label>
@@ -125,7 +74,7 @@
 
                 <button type="submit" class=" contact100-form-btn ">{{ __('messages.registrati') }}
                 </button>
-                
+
             </form>
 
         </div>
